4cf0644a442918883f2f2a40c9099578546480e9
Collin Morudi <collinmorudi@gmail.com>
2024-11-05 23:39:43 +0200

    add: 0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""0-app.py"""
+
+
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    """Renders the homepage with a welcome message."""
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
