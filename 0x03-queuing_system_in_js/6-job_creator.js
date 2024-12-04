082d49578cd9c10f539741e03c156f526b6dc6fa
Collin Morudi <collinmorudi@gmail.com>
2024-12-04 13:28:26 +0200

    feat: Create job processor with Kue

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -4,7 +4,7 @@ const queue = kue.createQueue();
 
 const jobData = {
   phoneNumber: '1234567890',
-  message: 'This is a notification message',
+  message: 'This is the code to verify your account',
 };
 
 const job = queue.create('push_notification_code', jobData)
