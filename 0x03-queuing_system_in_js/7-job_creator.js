72bd472d24f8a7f88293746554fac7af4d6f02f8
Collin Morudi <collinmorudi@gmail.com>
2024-12-04 13:41:22 +0200

    feat: Create job processor with error handling and progress tracking

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -2,70 +2,20 @@ import kue from 'kue';
 
 const queue = kue.createQueue();
 
-const jobs = [
-  {
-    phoneNumber: '4153518780',
-    message: 'This is the code 1234 to verify your account',
-  },
-  {
-    phoneNumber: '4153518781',
-    message: 'This is the code 4562 to verify your account',
-  },
-  {
-    phoneNumber: '4153518743',
-    message: 'This is the code 4321 to verify your account',
-  },
-  {
-    phoneNumber: '4153538781',
-    message: 'This is the code 4562 to verify your account',
-  },
-  {
-    phoneNumber: '4153118782',
-    message: 'This is the code 4321 to verify your account',
-  },
-  {
-    phoneNumber: '4153718781',
-    message: 'This is the code 4562 to verify your account',
-  },
-  {
-    phoneNumber: '4159518782',
-    message: 'This is the code 4321 to verify your account',
-  },
-  {
-    phoneNumber: '4158718781',
-    message: 'This is the code 4562 to verify your account',
-  },
-  {
-    phoneNumber: '4153818782',
-    message: 'This is the code 4321 to verify your account',
-  },
-  {
-    phoneNumber: '4154318781',
-    message: 'This is the code 4562 to verify your account',
-  },
-  {
-    phoneNumber: '4151218782',
-    message: 'This is the code 4321 to verify your account',
-  },
-];
+const blacklistedNumbers = ['4153518780', '4153518781'];
 
-jobs.forEach((jobData) => {
-  const job = queue.create('push_notification_code_2', jobData)
-    .save((err) => {
-      if (!err) {
-        console.log(`Notification job created: ${job.id}`);
-      }
-    });
+function sendNotification(phoneNumber, message, job, done) {
+  job.progress(0, 100);
+  if (blacklistedNumbers.includes(phoneNumber)) {
+    done(Error(`Phone number ${phoneNumber} is blacklisted`));
+    return;
+  }
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
+}
 
-  job.on('complete', () => {
-    console.log(`Notification job ${job.id} completed`);
-  });
-
-  job.on('failed', (errorMessage) => {
-    console.log(`Notification job ${job.id} failed: ${errorMessage}`);
-  });
-
-  job.on('progress', (progress) => {
-    console.log(`Notification job ${job.id} ${progress}% complete`);
-  });
+queue.process('push_notification_code_2', 2, (job, done) => {
+  const { phoneNumber, message } = job.data;
+  sendNotification(phoneNumber, message, job, done);
 });
